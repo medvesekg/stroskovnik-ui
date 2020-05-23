951ba6601e96eb239a0edb7af4ebfe66476e625e
Gregor Medvesek <medvesek.gregor@gmail.com>
2020-05-23 10:19:41 +0200

    Handle case when there is no cookie

--- store/index.js
+++ store/index.js
@@ -3,14 +3,16 @@ const cookieParser = require('cookie')
 
 export const actions = {
   nuxtServerInit({ commit }, { req }) {
-    const cookie = cookieParser.parse(req.headers.cookie)
-    const token = cookie ? cookie['apollo-token'] : null
-    if (token) {
-      const decodedToken = jwtDecode(token)
-      commit('auth/SET_TOKEN', token)
-      const hasuraClaim = decodedToken['https://hasura.io/jwt/claims']
-      commit('auth/SET_ROLE', hasuraClaim['x-hasura-default-role'])
-      commit('auth/SET_USER', decodedToken)
+    if (req.headers.cookie) {
+      const cookie = cookieParser.parse(req.headers.cookie)
+      const token = cookie ? cookie['apollo-token'] : null
+      if (token) {
+        const decodedToken = jwtDecode(token)
+        commit('auth/SET_TOKEN', token)
+        const hasuraClaim = decodedToken['https://hasura.io/jwt/claims']
+        commit('auth/SET_ROLE', hasuraClaim['x-hasura-default-role'])
+        commit('auth/SET_USER', decodedToken)
+      }
     }
   }
 }
