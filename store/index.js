7df735348044a6a7d84dbc8fef7d2ab58dd7fce0
Gregor Medvesek <medvesek.gregor@gmail.com>
2020-05-23 14:45:04 +0200

    Authenticate apollo server side

--- store/index.js
+++ store/index.js
@@ -2,13 +2,14 @@ import jwtDecode from 'jwt-decode'
 const cookieParser = require('cookie')
 
 export const actions = {
-  nuxtServerInit({ commit }, { req }) {
+  nuxtServerInit({ commit }, { req, app }) {
     if (req.headers.cookie) {
       const cookie = cookieParser.parse(req.headers.cookie)
       const token = cookie ? cookie['apollo-token'] : null
       if (token) {
         const decodedToken = jwtDecode(token)
         commit('auth/SET_TOKEN', token)
+        app.$apolloHelpers.onLogin(token)
         const hasuraClaim = decodedToken['https://hasura.io/jwt/claims']
         commit('auth/SET_ROLE', hasuraClaim['x-hasura-default-role'])
         commit('auth/SET_USER', { ...decodedToken, uid: decodedToken.user_id })
